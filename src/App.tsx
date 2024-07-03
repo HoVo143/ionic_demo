640ac4bfedf49a8f8f497dad1d2b623db974172d
hovo <adadad077@gmail.com>
2024-07-03 10:18:35 +0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -19,19 +19,11 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+// import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Details from './pages/Details';
 
 setupIonicReact();
 
@@ -39,11 +31,10 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
+        <Route exact path="/movies" component={Home}></Route>
+        <Route exact path="/movies/:id" component={Details}></Route>
         <Route exact path="/">
-          <Redirect to="/home" />
+          <Redirect to="/movies" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,55 @@
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode',
+}
+
+export interface SearchResult {
+  Title: string
+  Year: string
+  Poster: string
+  imdbID: string
+  Type: string
+}
+
+export interface SearchError {
+  Response: string
+  Error: string
+}
+
+export interface DetailsResult {
+  Genre: string
+  Title: string
+  Year: string
+  Poster: string
+  Plot: string
+  imdbRating: string
+  Director: string
+  Actors: string
+  Website:string
+  Awards:string
+}
+
+export const useApi = () =>{
+  // https://www.omdbapi.com/?i=tt3896198&apikey=7fddd07f
+  let url = 'https://www.omdbapi.com/'
+  let apiKey = '7fddd07f'
+
+  const searchData = async (title:string, type: SearchType) : Promise<SearchResult[] | SearchError > => {
+    const result = await fetch(
+      `${url}?s=${encodeURI(title)}&type=${type}&apiKey=${apiKey}`
+    )
+    return result.json()
+  }
+
+  const getDetails = async(id: string): Promise<DetailsResult> =>{
+    const result = await fetch(`${url}?i=${(id)}&plot=full&apiKey=${apiKey}`)
+    return result.json()
+  }
+
+  return {
+    searchData,
+    getDetails
+  }
+}
