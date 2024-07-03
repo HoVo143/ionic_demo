1cfdd8d66effecc42857244e6ea00d2d436cb0ee
hovo <adadad077@gmail.com>
2024-07-03 10:29:11 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     react(),
     legacy()
   ],
+  build: {
+    outDir: 'dist', // Specify your desired output directory here
+  },
   test: {
     globals: true,
     environment: 'jsdom',
